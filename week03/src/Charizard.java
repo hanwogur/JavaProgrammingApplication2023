a2755c16d23320b7c7f3b54cddb07aa81c51b2e3
hanwogur <wogur110507@gmail.com>
2023-09-13 15:45:04 +0900

    week03 v12

--- week03/src/Charizard.java
+++ week03/src/Charizard.java
@@ -2,7 +2,7 @@ public class Charizard extends Pokemon{
     public Charizard() {
         System.out.println("자식 클래스(리자몽)의 기본 생성자");
         this.level = 36;
-//      this.setLevel(36);
+//      this.setLevel(36);dsa
         this.setHp(78);
         this.name = "리자몽";
     }
