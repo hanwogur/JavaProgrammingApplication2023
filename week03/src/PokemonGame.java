b0076a8e9693bea3727b5d745f1d569b5f90e774
hanwogur <wogur110507@gmail.com>
2023-09-13 15:10:38 +0900

    week03 v1

--- week03/src/PokemonGame.java
+++ week03/src/PokemonGame.java
@@ -1,32 +1,19 @@
-
 public class PokemonGame {
     public static void main(String[] args) {
-        Pokemon squirtle = new Pokemon("꼬부기", 1, 50);
-        Pokemon pikachu = new Pokemon("피카츄");
+        Pokemon pikachu = new Pokemon();
         Pokemon charizard = new Pokemon();
 
-        squirtle.evolve(squirtle);
-        System.out.println(squirtle.getName());
-        squirtle.evolve(squirtle);
+        pikachu.name = "피카츄";
+        charizard.name = "리자몽";
 
-//        pikachu.name = "피카츄";
-//        charizard.name = "리자몽";
+        pikachu.level = 3;
+        pikachu.hp = 100;
 
-        //pikachu.setName("피카츄");
-        charizard.setName("리자몽");
+        charizard.level = 36;
+        charizard.hp = 800;
 
-        pikachu.setLevel(23);
-        pikachu.setHp(100);
-
-        charizard.setLevel(36);
-        charizard.setHp(800);
-
-        System.out.println(charizard.getName() + "의 체력은 " + charizard.getHp() + "이고 레벨은 " + charizard.getLevel() + "입니다");
-        charizard.evolve(charizard);
-        pikachu.evolve(pikachu);
-        pikachu.evolve(pikachu);
-        pikachu.setName("라이츄");
-        System.out.println(pikachu.getName());
-        pikachu.evolve(pikachu);
+        System.out.println(charizard.name + "의 체력은 " + charizard.hp + "이고 레벨은 " + charizard.level + "입니다");
+        charizard.evolve(charizard.level);
+        pikachu.evolve(pikachu.level);
     }
 }
