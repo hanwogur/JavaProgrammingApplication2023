9d8403e5899d5cf9197ff732dc311605491fdbc2
hanwogur <wogur110507@gmail.com>
2023-09-20 15:50:14 +0900

    week4 v22

--- week04/src/Pokemon.java
+++ week04/src/Pokemon.java
@@ -1,5 +1,5 @@
 
-public abstract class Pokemon {
+public abstract class Pokemon {  // 추상적 클래스
     //private int level;
     protected int level;
     private int hp;
@@ -52,6 +52,10 @@ public abstract class Pokemon {
     }
 
     public abstract void attack();
+    public void attack(Pokemon targetPokemon){
+        System.out.println(this.name+"이(가) "+targetPokemon.name+"에게 공격을 합니다.");
+        //targetPokemon.hp = this.
+    }
     public void evolve(){  // 매개변수 제거
         if(this instanceof Pikachu){
             System.out.println("삐까삐까~");
--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -1,4 +1,5 @@
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class PokemonGame {
     public static void main(String[] args) {
@@ -8,19 +9,49 @@ public class PokemonGame {
 //        System.out.println((int)(Math.random()*6)+1); // 1 <= x <= 6
 
         // 적군 포켓몬 랜덤 생성
+        Pokemon enemy = null;
         int enemyPick = (int)(Math.random()*3);
         if(enemyPick == 0){
             NoFly noFly = new NoFly();
-            Pikachu pikachu = new Pikachu(noFly);
+            enemy = new Pikachu(noFly);
         } else if(enemyPick == 1){
             NoFly noFly = new NoFly();
-            Squirtle squirtle = new Squirtle(noFly);
+            enemy = new Squirtle(noFly);
         } else if(enemyPick == 2){
             Wings wings = new Wings();
-            Charizard charizard = new Charizard(wings);
+            enemy = new Charizard(wings);
         } else{
             System.out.println("여기는 영원히 실행 안됩니다.");
         }
 
+        //플레이어 포켓몬 선택
+        //Pokemon player = new Pokemon(); 추상클래스의 객체는 생성 불가
+        Pokemon player = null; // 추상클래스의 변수 선언은 가능 (upcasting 용)
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("포켓몬을 고르세요.\n1) 피카츄   2) 꼬부기   3) 리자몽");
+        int pokemonPick = scanner.nextInt();
+        if(pokemonPick == 1){
+            player = new Pikachu(new NoFly());
+        } else if(pokemonPick == 2){
+            player = new Squirtle(new NoFly());
+        } else if(pokemonPick == 3){
+            player = new Charizard(new Wings());
+        } else {
+            System.out.println("정상적인 값이 아닙니다.");
+        }
+
+        int menu;
+        while(true){
+            System.out.print("\n1) 전투   2) 도망   3) 종료 : ");
+            menu = scanner.nextInt();
+            if(menu == 1){
+                player.attack(enemy);
+            } else if(menu == 2){
+
+            } else {
+                System.out.println("게임을 종료합니다");
+                break;
+            }
+        }
     }
 }
