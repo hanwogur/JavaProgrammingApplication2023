e5b006de7a364324630c7b22f9e912b3fa55db68
hanwogur <wogur110507@gmail.com>
2023-09-20 14:04:49 +0900

    week4 v18

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -3,8 +3,8 @@ public class PokemonGame {
     public static void main(String[] args) {
 
         Pikachu pikachu = new Pikachu();
-        pikachu.setFlyable(new NoFly());  // 프로그램 실행 중 동적으로 나는 방식을 변경
-        pikachu.performFly();
+        //pikachu.setFlyable(new NoFly());  // 프로그램 실행 중 동적으로 나는 방식을 변경
+        pikachu.performFly(); // Association (Composition)
 
     }
 }
