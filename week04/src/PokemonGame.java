f54553a0489e301b81b8d3949376dbfdb74d019a
hanwogur <wogur110507@gmail.com>
2023-09-20 14:42:29 +0900

    week4 v19

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -2,9 +2,11 @@
 public class PokemonGame {
     public static void main(String[] args) {
 
-        Pikachu pikachu = new Pikachu();
+        NoFly noFly = new NoFly();
+        Pikachu pikachu = new Pikachu(noFly);  // Association, Has -a (Aggregation)  한쪽만 사라지고 남은 하나는 다른곳에 사용되는 것
         //pikachu.setFlyable(new NoFly());  // 프로그램 실행 중 동적으로 나는 방식을 변경
-        pikachu.performFly(); // Association (Composition)
+
+        pikachu.performFly(); // Association (Composition) 같이 사라지는것
 
     }
 }
